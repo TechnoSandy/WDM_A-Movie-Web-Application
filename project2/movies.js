d087f6132ce9bacb98f4d99a293a2c6a54c7447d
Techno Sandy <technosandyindia007@gmail.com>
2018-09-27 15:20:53 -0500

    updated project work

--- project2/movies.js
+++ project2/movies.js
@@ -1,17 +1,34 @@
-function initialize () {
+//299536
+//
+//24428
+//
+//99861
+//
+//9320
+
+
+function initialize() {}
+
+function sendRequest() {
+	var xhr = new XMLHttpRequest();
+	var query = encodeURI(document.getElementById("form-input").value);
+	//	xhr.open("GET", "proxy.php?method=/3/search/movie&query=" + query);
+	xhr.open("GET", "proxy.php?method=/3/movie/" + query);
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.onreadystatechange = function () {
+		if (this.readyState == 4) {
+			var json = JSON.parse(this.responseText);
+			console.log(json);
+			var str = JSON.stringify(json, undefined, 2);
+			//          document.getElementById("output").innerHTML = "<pre>" + str + "</pre>";
+			addMovieList(str);
+		}
+	};
+	xhr.send(null);
+
 }
 
-function sendRequest () {
-   var xhr = new XMLHttpRequest();
-   var query = encodeURI(document.getElementById("form-input").value);
-   xhr.open("GET", "proxy.php?method=/3/search/movie&query=" + query);
-   xhr.setRequestHeader("Accept","application/json");
-   xhr.onreadystatechange = function () {
-       if (this.readyState == 4) {
-          var json = JSON.parse(this.responseText);
-          var str = JSON.stringify(json,undefined,2);
-          document.getElementById("output").innerHTML = "<pre>" + str + "</pre>";
-       }
-   };
-   xhr.send(null);
+function addMovieList(str) {
+	document.getElementById("outputMSG").innerHTML = "<pre>" + str + "</pre>";
+	document.getElementById("output").innerHTML = "addMovieList() called";
 }
